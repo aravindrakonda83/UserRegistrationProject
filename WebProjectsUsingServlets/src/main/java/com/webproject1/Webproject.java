99cafe67567202b18380e753c72faa7f16c13104
arvin <arvin@Aravind_Rakonda>
2025-01-29 09:20:47 +0530

    Initial Commit

---
+++ WebProjectsUsingServlets/src/main/java/com/webproject1/Webproject.java
@@ -0,0 +1,29 @@
+package com.webproject1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Webproject")
+public class Webproject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter pw = response.getWriter();
+		response.setContentType("text/html");
+		pw.println("<h1> Program is running....gg </h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
